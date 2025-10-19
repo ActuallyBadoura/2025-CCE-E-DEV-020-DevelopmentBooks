ba403ea156f4ff8d0210b17d2fd66fb812090e88
Badreddine Hachoumi <badreddine.hachoumi@gmail.com>
2025-10-19 21:26:44 +0200

    removed the swagger, nice to have but not needed
    Signed-off-by: Badreddine Hachoumi <badreddine.hachoumi@gmail.com>

--- src/main/java/com/kata/developmentbooks/AppInitializer.java
+++ src/main/java/com/kata/developmentbooks/AppInitializer.java
@@ -1,5 +1,6 @@
 package com.kata.developmentbooks;
 
+import com.kata.developmentbooks.config.AppConfig;
 import com.kata.developmentbooks.config.WebConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
@@ -7,7 +8,7 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return null;
+        return new Class[] {AppConfig.class};
     }
 
     @Override
