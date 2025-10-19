95e6a40a183647f48391972fabfa51444b61177e
Badreddine Hachoumi <badreddine.hachoumi@gmail.com>
2025-10-19 19:59:13 +0200

    adding more tests for bigger baskets
    Signed-off-by: Badreddine Hachoumi <badreddine.hachoumi@gmail.com>

--- src/test/java/com/kata/developmentbooks/service/BookStoreServiceTest.java
+++ src/test/java/com/kata/developmentbooks/service/BookStoreServiceTest.java
@@ -50,4 +50,87 @@ public class BookStoreServiceTest {
             // Then
             assertThat(price).isEqualTo(new BigDecimal("50.00"));
         }
+
+    @Test
+    void testTwoSameBooks() {
+        // Given
+        List<Integer> basket = Arrays.asList(CLEAN_CODE, CLEAN_CODE);
+
+        // When
+        BigDecimal price = bookStoreService.calculatePrice(basket);
+
+        // Then
+        assertThat(price).isEqualTo(new BigDecimal("100.00"));
+    }
+
+    @Test
+    void testTwoDifferentBooks() {
+        // Given - 5% discount
+        List<Integer> basket = Arrays.asList(CLEAN_CODE, CLEAN_CODER);
+
+        // When
+        BigDecimal price = bookStoreService.calculatePrice(basket);
+
+        // Then
+        // 2 * 50 * 0.95 = 95
+        assertThat(price).isEqualTo(new BigDecimal("95.00"));
+    }
+
+    @Test
+    void testThreeDifferentBooks() {
+        // Given - 10% discount
+        List<Integer> basket = Arrays.asList(CLEAN_CODE, CLEAN_CODER, CLEAN_ARCHITECTURE);
+
+        // When
+        BigDecimal price = bookStoreService.calculatePrice(basket);
+
+        // Then
+        // 3 * 50 * 0.90 = 135
+        assertThat(price).isEqualTo(new BigDecimal("135.00"));
+    }
+
+    @Test
+    void testFourDifferentBooks() {
+        // Given - 20% discount
+        List<Integer> basket = Arrays.asList(
+                CLEAN_CODE, CLEAN_CODER, CLEAN_ARCHITECTURE, TDD_BY_EXAMPLE
+        );
+
+        // When
+        BigDecimal price = bookStoreService.calculatePrice(basket);
+
+        // Then
+        // 4 * 50 * 0.80 = 160
+        assertThat(price).isEqualTo(new BigDecimal("160.00"));
+    }
+
+    @Test
+    void testFiveDifferentBooks() {
+        // Given - 25% discount
+        List<Integer> basket = Arrays.asList(
+                CLEAN_CODE, CLEAN_CODER, CLEAN_ARCHITECTURE, TDD_BY_EXAMPLE, LEGACY_CODE
+        );
+
+        // When
+        BigDecimal price = bookStoreService.calculatePrice(basket);
+
+        // Then
+        // 5 * 50 * 0.75 = 187.50
+        assertThat(price).isEqualTo(new BigDecimal("187.50"));
+    }
+
+    @Test
+    void testMixedBasket() {
+        // Given
+        List<Integer> basket = Arrays.asList(
+                CLEAN_CODE, CLEAN_CODE, CLEAN_CODER
+        );
+
+        // When
+        BigDecimal price = bookStoreService.calculatePrice(basket);
+
+        // Then
+        // (2 * 50 * 0.95) + 50 = 95 + 50 = 145
+        assertThat(price).isEqualTo(new BigDecimal("145.00"));
+    }
 }
